67f5135c7b3027f2269bfd98306bd77c5eb9ad00
Andreas Loukakis <alou@alou.gr>
2014-08-01 15:57:23 +0300

    Update routes.php

--- routes.php
+++ routes.php
@@ -7,10 +7,12 @@ Route::get('/', array('as' => 'home',
  'uses' => 'App\Site\Controllers\GetcontentController@index')
 );
 
+//normal post route, in case js is disabled
 Route::post('vfind', array('before' => 'csrf', 'as' => 'postvid', 
  'uses' => 'App\Site\Controllers\GetcontentController@getUrl')
 );
 
+//ajax post route
 Route::post('a/vfind', array('before' => 'csrf', 'as' => 'ajaxpostvid', 
  'uses' => 'App\Site\Controllers\GetcontentController@getAjaxUrl')
 );
